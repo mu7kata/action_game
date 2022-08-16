147fc98a507112634966ba5befaa718e1a7bd284
宗像勇樹 <munakatayuki@munekatayuukinoMacBook-Pro.local>
2022-08-16 13:21:36 +0900

    キャラ追加（eda）

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -3,6 +3,7 @@
     <h1>選択画面</h1>
     <input type="button" @click="player = 'kaki'" value="カッキー"/>
     <input type="button" @click="player = 'haru'" value="はるか"/>
+    <input type="button" @click="player = 'eda'" value="枝松"/>
     <router-link :to="`battle/${player}`">battle</router-link>
   </div>
 </template>
