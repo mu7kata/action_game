f8b96c7f383a23c07f197dbc9459650f020de0ea
宗像勇樹 <munakatayuki@munekatayuukinoMacBook-Pro.local>
2022-08-16 23:06:11 +0900

    すーさん追加

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -4,6 +4,7 @@
     <input type="button" @click="player = 'kaki'" value="カッキー"/>
     <input type="button" @click="player = 'haru'" value="はるか"/>
     <input type="button" @click="player = 'eda'" value="枝松"/>
+    <input type="button" @click="player = 'kuni'" value="邦明"/>
     <router-link :to="`battle/${player}`">battle</router-link>
   </div>
 </template>
